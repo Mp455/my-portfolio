692b1b7fcc6e243b48770e221f1a9989d1000808
Marcos Paulo <marcospaulosousa48@gmail.com>
2023-11-20 12:30:27 -0300

    feat: create button component

---
+++ app/components/Button/index.tsx
@@ -0,0 +1,19 @@
+import { ButtonHTMLAttributes } from "react";
+import { cn } from "@/app/lib/utils";
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+export const Button = ({ children, className, ...props }: ButtonProps) => {
+  return (
+    <button
+      className={cn(
+        "bg-red-600 py-3 px-4 rounded-lg text-white flex items-center justify-center gap-2 hover:bg-red-900/80 transition-all disabled:opacity-50",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
