65b143b1b3fa519ec758ee08572a793891c05165
Marcos Paulo <marcospaulosousa48@gmail.com>
2023-11-20 14:17:24 -0300

    feat: add new tecnologies

--- app/components/pages/KnowTechs/index.tsx
+++ app/components/pages/KnowTechs/index.tsx
@@ -1,20 +1,74 @@
 import SectionTitle from "../../SectionTitle";
 import KnowTech from "./KnowTech";
-import { TbBrandNextjs } from "react-icons/tb";
-
+import { TbBrandNextjs, TbBrandTypescript } from "react-icons/tb";
+import { FaNode } from "react-icons/fa";
+import { FaReact } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { SiMongodb } from "react-icons/si";
+import { DiPostgresql } from "react-icons/di";
+import { IoLogoCss3 } from "react-icons/io";
+import { FaGitSquare } from "react-icons/fa";
+import { SiTailwindcss } from "react-icons/si";
+import { IoLogoJavascript } from "react-icons/io";
+import { SiStyledcomponents } from "react-icons/si";
 export const KnowTechs = () => {
+  const MOCK_TECNOLOGIES = [
+    {
+      icon: <FaHtml5 />,
+      name: "HTML",
+    },
+    {
+      icon: <IoLogoCss3 />,
+      name: "CSS",
+    },
+    {
+      icon: <IoLogoJavascript />,
+      name: "JavaScript",
+    },
+    {
+      icon: <FaReact />,
+      name: "React",
+    },
+    {
+      icon: <TbBrandNextjs />,
+      name: "Next.Js",
+    },
+    {
+      icon: <TbBrandTypescript />,
+      name: "Typescript",
+    },
+    {
+      icon: <FaNode />,
+      name: "Node.Js",
+    },
+    {
+      icon: <SiTailwindcss />,
+      name: "TailwindCSS",
+    },
+    {
+      icon: <SiStyledcomponents />,
+      name: "Styled Components",
+    },
+    {
+      icon: <SiMongodb />,
+      name: "MongoDB",
+    },
+    {
+      icon: <DiPostgresql />,
+      name: "PostgreSQL",
+    },
+    {
+      icon: <FaGitSquare />,
+      name: "Versionamento GIT",
+    },
+  ];
+
   return (
     <section className="container py-16">
       <SectionTitle subtitle="Competências" title="Conhecimentos" />
       <div className="grid grid-cols-[repeat(auto-fit,minmax(265px,1fr))] gap-3 mt-[50px]">
-        {Array.from({ length: 8 }).map((_, index) => (
-          <KnowTech
-            key={index}
-            tech={{
-              icon: <TbBrandNextjs />,
-              name: "Next.Js",
-            }}
-          />
+        {MOCK_TECNOLOGIES.map((tech, index) => (
+          <KnowTech key={index} tech={tech} />
         ))}
       </div>
     </section>
---
+++ app/components/pages/home/HighlightedProjects/index.tsx
@@ -0,0 +1,11 @@
+import SectionTitle from "@/app/components/SectionTitle";
+
+export const HighlightedProjects = () => {
+  return (
+    <section className="container py-16">
+      <SectionTitle subtitle="Destaques" title="Projetos em Destaque" />
+    </section>
+  );
+};
+
+export default HighlightedProjects;
