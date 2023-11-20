72010145997dff4bde56d974dd35773a2d5eaf2c
Marcos Paulo <marcospaulosousa48@gmail.com>
2023-11-20 14:32:44 -0300

    feat: create horizontal row divider component

--- app/components/pages/home/HighlightedProjects/index.tsx
+++ app/components/pages/home/HighlightedProjects/index.tsx
@@ -1,9 +1,11 @@
+import HorizontalDivider from "@/app/components/Divider/horizontal";
 import SectionTitle from "@/app/components/SectionTitle";
 
 export const HighlightedProjects = () => {
   return (
     <section className="container py-16">
       <SectionTitle subtitle="Destaques" title="Projetos em Destaque" />
+      <HorizontalDivider className="mb-16" />
     </section>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import KnowTechs from "./components/pages/KnowTechs";
 import HeroSection from "./components/pages/home/HeroSection";
+import HighlightedProjects from "./components/pages/home/HighlightedProjects";
 
 export default async function Home() {
   return (
     <>
       <HeroSection />
       <KnowTechs />
+      <HighlightedProjects />
     </>
   );
 }
