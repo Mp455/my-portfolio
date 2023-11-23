dddf780a7fc8fbe2593d648a9ac38ca259bca19e
Marcos Paulo <marcospaulosousa48@gmail.com>
2023-11-23 17:09:39 -0300

    feat: add typing props

--- app/types/projects.ts
+++ app/types/projects.ts
@@ -1,4 +1,29 @@
+import type { RichTextContent } from "@graphcms/rich-text-types";
+
 export type KnownTech = {
   iconSvg: string;
   name: string;
+};
+
+export type ProjectSection = {
+  title: string;
+  image: {
+    url: string;
+  };
+};
+
+export type Project = {
+  slug: string;
+  thumbnail: {
+    url: string;
+  };
+  title: string;
+  shortDescription: string;
+  technologies: KnownTech[];
+  sections: ProjectSection[];
+  description: {
+    raw: RichTextContent;
+  };
+  githubUrl?: string;
+  liveProjectUrl?: string;
 };
