d6e83f5dc8484a5143a170af1543d77fead39f1c
Marcos Paulo <marcospaulosousa48@gmail.com>
2023-11-25 22:22:55 -0300

    feat: add fade Up Animation props

--- app/components/pages/home/WorkExperience/ExperienceItem.tsx
+++ app/components/pages/home/WorkExperience/ExperienceItem.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 import { differenceInMonths, differenceInYears, format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import { motion } from "framer-motion";
-import { techBadgeAnimation } from "@/app/lib/animations";
+import { fadeUpAnimation, techBadgeAnimation } from "@/app/lib/animations";
 type ExperienceItemProps = {
   experience: workExperience;
 };
@@ -48,10 +48,7 @@ export const ExperienceItem = ({ experience }: ExperienceItemProps) => {
   return (
     <motion.div
       className="grid grid-cols-[40px,1fr] gap-4 md:gap-10"
-      initial={{ opacity: 0, y: 50 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: 50 }}
-      transition={{ duration: 0.5 }}
+      {...fadeUpAnimation}
     >
       <div className="flex flex-col items-center gap-4">
         <div className="rounded-full border border-gray-500 p-0.5">
