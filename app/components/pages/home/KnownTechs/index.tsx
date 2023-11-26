7015307d69655dbaea87d2ff238eb663bd87e52f
Marcos Paulo <marcospaulosousa48@gmail.com>
2023-11-25 22:23:29 -0300

    style: add while in view to known techs

--- app/components/pages/home/KnownTechs/index.tsx
+++ app/components/pages/home/KnownTechs/index.tsx
@@ -14,7 +14,7 @@ export const KnownTechs = ({ techs }: KnownTechProps) => {
         {techs?.map((tech, i) => (
           <motion.div
             initial={{ opacity: 0, x: -100 }}
-            animate={{ opacity: 1, x: 0 }}
+            whileInView={{ opacity: 1, x: 0 }}
             exit={{ opacity: 0, x: -100 }}
             transition={{ duration: 0.15, delay: i * 0.1 }}
           >
