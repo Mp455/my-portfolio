d7c1c2170502ba6974fb73924975fa3d931f3b3f
Marcos Paulo <marcospaulosousa48@gmail.com>
2023-11-25 21:31:56 -0300

    feat: add animation to known techs component

--- app/components/pages/home/KnownTechs/index.tsx
+++ app/components/pages/home/KnownTechs/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { motion } from "framer-motion";
 import SectionTitle from "../../../SectionTitle";
 import KnownTech from "./KnownTech";
 import { KnownTech as IKnownTech } from "@/app/types/projects";
@@ -9,8 +11,15 @@ export const KnownTechs = ({ techs }: KnownTechProps) => {
     <section className="container py-16">
       <SectionTitle subtitle="Competências" title="Conhecimentos" />
       <div className="grid grid-cols-[repeat(auto-fit,minmax(265px,1fr))] gap-3 mt-[50px]">
-        {techs?.map((tech) => (
-          <KnownTech key={tech.name} tech={tech} />
+        {techs?.map((tech, i) => (
+          <motion.div
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -100 }}
+            transition={{ duration: 0.15, delay: i * 0.1 }}
+          >
+            <KnownTech key={tech.name} tech={tech} />
+          </motion.div>
         ))}
       </div>
     </section>
