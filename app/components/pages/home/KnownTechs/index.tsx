2d7fdc312dfba2bcd4e8d528aa15da14d3366dca
Marcos Paulo <marcospaulosousa48@gmail.com>
2023-11-27 10:05:07 -0300

    feat: add config to deploy

--- app/components/pages/home/KnownTechs/index.tsx
+++ app/components/pages/home/KnownTechs/index.tsx
@@ -13,12 +13,13 @@ export const KnownTechs = ({ techs }: KnownTechProps) => {
       <div className="grid grid-cols-[repeat(auto-fit,minmax(265px,1fr))] gap-3 mt-[50px]">
         {techs?.map((tech, i) => (
           <motion.div
+            key={tech.name}
             initial={{ opacity: 0, x: -100 }}
             whileInView={{ opacity: 1, x: 0 }}
             exit={{ opacity: 0, x: -100 }}
             transition={{ duration: 0.15, delay: i * 0.1 }}
           >
-            <KnownTech key={tech.name} tech={tech} />
+            <KnownTech tech={tech} />
           </motion.div>
         ))}
       </div>
